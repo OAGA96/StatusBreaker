594ad9f64c3d9383acf3e11ca874d60525758196
Oscar Garcia <you@example.com>
2020-06-04 12:36:17 -0700

    Compatibilidad IE 9

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(
   <React.StrictMode>
